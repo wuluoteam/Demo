cb3fe113ada9fd47aa61afbe970dfb13686cfb17
wuluoteam <1337843618@qq.com>
2014-09-23 17:48:23 +0800

    1.0

--- src/com/project/savemoney/WelcomActivity.java
+++ src/com/project/savemoney/WelcomActivity.java
@@ -1,7 +1,9 @@
 package com.project.savemoney;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Window;
 
 public class WelcomActivity  extends Activity{
@@ -11,7 +13,14 @@ public class WelcomActivity  extends Activity{
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.welcome);
-		
+		new Handler().postDelayed(new Runnable() {
+			public void run() {
+				Intent intent = new Intent(WelcomActivity.this,
+						MainActivity.class); // Ö÷½çÃæ
+				startActivity(intent);
+				WelcomActivity.this.finish();
+			}
+		}, 3000);
 		
 	}
 }
